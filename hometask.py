dfa5e065c5aba79c1592e14888c811208c7b5cfe
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-07 14:34:13 +0300

    done

--- hometask.py
+++ hometask.py
@@ -1,1 +1,12 @@
 
+numbers = [int(num) for num in input("input nums: ").split()]
+
+k = int(input("input  k: "))
+
+if len(numbers) < k:
+    print(" there are less than K")
+else:
+    numbers.sort(reverse=True)
+
+    result = numbers[k - 1]
+    print(int(k), " is the larest:", int(result))
