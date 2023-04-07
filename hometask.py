c2d2debe8ee1510bb5be74a2f0f7b1ebe3b7e990
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-07 14:19:11 +0300

    done_5

--- hometask.py
+++ hometask.py
@@ -1,1 +1,15 @@
 
+
+numbers = input("input numbers: ").split()
+
+
+numbers = list(map(int, numbers))
+
+positive_numbers = []
+
+
+for num in numbers:
+    if num > 0:
+        positive_numbers.append(num)
+
+print("list:", positive_numbers)
