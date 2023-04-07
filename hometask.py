9780b9d780d0a85093ac3c5edea7cca25977f9fd
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-07 14:26:30 +0300

    done

--- hometask.py
+++ hometask.py
@@ -1,1 +1,10 @@
 
+numbers = [int(num) for num in input("input nums: ").split()]
+
+min_idx = numbers.index(min(numbers))
+max_idx = numbers.index(max(numbers))
+
+numbers[min_idx], numbers[max_idx] = numbers[max_idx], numbers[min_idx]
+
+print("list:")
+print(numbers)
