b9a4f039ae62f4daa0f5f0338316eae375f7371b
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-07 14:41:29 +0300

    done

--- hometask.py
+++ hometask.py
@@ -1,1 +1,8 @@
 
+filter_strings_with_prefix = lambda strings, prefix: [s for s in strings if s.startswith(prefix)]
+
+strings = ["apple", "banana", "cherry", "date", "kiwi"]
+prefix = "b"
+filtered_list = filter_strings_with_prefix(strings, prefix)
+print("words started with '{}':".format(prefix))
+print(filtered_list)
