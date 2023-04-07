313aef48b03c8f7ef0be8ef40c3f819a13fd9e07
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-07 13:57:26 +0300

    done1

--- hometask.py
+++ hometask.py
@@ -1,1 +1,22 @@
 
+import random
+
+n = int(input("input amount of num: "))
+
+numbers = []
+for i in range(n):
+    numbers.append(random.randint(1, 12)) 
+
+print("list:", numbers)
+
+even_count = 0
+odd_count = 0
+
+for number in numbers:
+    if number % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("even nums:", even_count)
+print("odd nums:", odd_count)
