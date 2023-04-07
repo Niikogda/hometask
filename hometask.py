3f617830fe0f6422c33efdeef9625f1a9b19a56a
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-07 14:36:58 +0300

    done

--- hometask.py
+++ hometask.py
@@ -1,1 +1,12 @@
 
+numbers = [int(num) for num in input("input nums: ").split()]
+
+average = sum(numbers) / len(numbers)
+
+count = 0
+
+for num in numbers:
+    if num > average:
+        count += 1
+
+print(" amount of nums, mor than the average:", count)
